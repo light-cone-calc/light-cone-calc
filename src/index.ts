90efaf8997cd56e4a94cb17ccedfd64c005041a9
pbuk <pb@pbuk.uk>
2022-05-21 02:41:30 +0100

    Release v.1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { calculateExpansion } from './expansion';
 export { Calculate, CalculateTage, ScaleResults } from './legacy';
 
-export const version = '1.2.0-dev.1';
+export const version = '1.2.0';
